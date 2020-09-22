f52a3cd5bcf59bc485bea5d0b0a5383da175f2bc
Prakash Kumar Singh <prakashs0990.ps@gmail.com>
2020-09-22 19:04:56 +0530

    many changes done

--- newDesign2.js
+++ newDesign2.js
@@ -5,57 +5,40 @@ var inputStar=document.getElementById("star");
 var inputTarget=document.getElementById("target");
 var inputTask=document.getElementById("task");
 
-var pipelineValue=document.getElementById("pipelineValue");
-var pipelineBar=document.getElementById("pipelineBar");
+var pipelineValue=document.getElementById("pipelineValue1");
+var pipelineBar=document.getElementById("pipelineBar1");
 var winsValue=document.getElementById("winsValue");
-var winsBar=document.getElementById("winsBar");
-var lossesValue=document.getElementById("lossesValue");
-var lossesBar=document.getElementById("lossesBar");
-var targetValue=document.getElementById("targetValue");
-var bullEye=document.getElementById("bullEye");
+var winsBar=document.getElementById("winsBar1");
+var lossesValue=document.getElementById("lossesValue1");
+var lossesBar=document.getElementById("lossesBar1");
+var targetValue=document.getElementById("targetValue1");
+var bullEye=document.getElementById("bullEye1");
 var taskValue=document.getElementById("taskValue");
 var taskImg=document.getElementById("taskImg");
 var percentage=document.getElementById("percentage");
-var percentageImg=document.getElementById("percentageImg");
+var percentageImg=document.getElementById("percentageImg1");
 var strategyYes=document.getElementById("bulbYes");
 var strategyNo=document.getElementById("bulb");
-var strategyImg=document.getElementById("strategyImg");
+var strategyImg=document.getElementById("strategyImg1");
 var competitorYes=document.getElementById("starYes");
-var competitorNo=document.getElementById("star");
-var competitorImg= document.getElementById("competitorImg");
+var competitorNo=document.getElementById("star1");
+var competitorImg= document.getElementById("competitorImg1");
 var status1=document.getElementById("status1");
 var status2=document.getElementById("status2");
 var status3= document.getElementById("status3");
 var status4=document.getElementById("status4");
-var box2=document.getElementById("box2");
+var box=document.getElementById("box1");
 var space1=document.getElementById("space1");
 var space2=document.getElementById("space2");
 var space3= document.getElementById("space3");
-var spaceTitle=document.getElementById("spaceTitle");
-var winContainer = document.getElementById("winContainer");
-var lossesContainer = document.getElementById("lossesContainer");
-var pipelineContainer=document.getElementById("pipelineContainer");
+var spaceTitle=document.getElementById("spaceTitle1");
+var winContainer = document.getElementById("winContainer1");
+var lossesContainer = document.getElementById("lossesContainer1");
+var pipelineContainer=document.getElementById("pipelineContainer1");
 var cube=document.getElementById("cube");
 var circle=document.getElementById("circle");
 var adjust=document.getElementById("adjust");
 var allContainer=document.getElementById("allContainer");
-
-var box1 = document.getElementById("box1");
-var competitorImg1=document.getElementById("competitorImg1");
-var spaceTitle1=document.getElementById("spaceTitle1");
-var strategyImg1=document.getElementById("strategyImg1");
-var pipelineValue1=document.getElementById("pipelineValue1");
-var pipelineBar1=document.getElementById("pipelineBar1");
-var bullEye1=document.getElementById("bullEye1");
-var winsValue1=document.getElementById("winsValue1");
-var winsBar1=document.getElementById("winsBar1");
-var targetValue1=document.getElementById("targetValue1");
-var lossesValue1=document.getElementById("lossesValue1");
-var lossesBar1=document.getElementById("lossesBar1");
-var percentageImg1=document.getElementById("percentageImg1");
-var percentage1=document.getElementById("percentage1");
-var taskImg1=document.getElementById("taskImg1");
-var taskValue1=document.getElementById("taskValue1");
 
 var colorStatus="#008000";
 var statusValue=status1;
@@ -64,80 +47,39 @@ tick1.style.display="block";
 
 
 //some prefilled value
-inputTarget.value=400000;
+function preFill(){
+    inputTarget.value=400000;
 inputWins.value=600000;
 inputPipeline.value=800000;
 inputLosses.value=200000;
 inputTask.value=05;
 strategyYes.value="select";
 competitorYes.value="chosen";
-//strategyYes.value="chosen";
-var template = document.getElementById("template").value;
+}
 
-var doc;
-var template = document.getElementById("template").value;
-            
-            $("#template").change(function(){
-    
-                
-                if($(this).val() == 'template1'){
-                    
-                    template="template1";
-                    document.getElementById("box1").style.display="block";
-                    document.getElementById("box2").style.display="none";
-                   
-                }
-                else{
-                    
-                    template="template2";
-                    document.getElementById("box1").style.display="none";
-                    document.getElementById("box2").style.display="block";
-                    
-    
-                }
-                
-                
-                
-            });
+//strategyYes.value="chosen";
+
 
 // Pipeline
 inputPipeline.onkeyup= function(){
     
     if(inputPipeline.value == '0' || inputPipeline.value < '1'){
         
-        if(template == "template1"){
-            pipelineValue1.innerHTML='&#8213';
-            pipelineValue1.style.color='#a0a2a4';
-            pipelineBar1.style.opacity='10%';
-            pipelineValue1.style.opacity='90%';
-        }
-        
-            //pipelineValue.innerHTML='NO PIPELINE';
+        //pipelineValue.innerHTML='NO PIPELINE';
         removeAll();
         pipelineValue.innerHTML='&#8213';
         pipelineValue.style.color='#a0a2a4';
         pipelineContainer.style.textAlign='center';
         pipelineValue.style.opacity='90%';
-        
-        
-        
+        pipelineBar.style.opacity='10%';
     }
     else{
-        
-        if(template=="template1"){
-            pipelineBar1.style.opacity='125%';
-            pipelineValue1.style.color='black';
-            pipelineValue1.innerHTML='PIPELINE | $'+ intToString( inputPipeline.value, 2);
-        }
-        
-            removeAll();
-            //pipelineBar.style.opacity='125%';
+        removeAll();
+        //pipelineBar.style.opacity='125%';
         //pipelineValue.innerHTML='PIPELINE | $'+ intToString( inputPipeline.value, 2);
         pipelineContainer.style.textAlign='start';
         pipelineValue.style.color='black';
         pipelineValue.innerHTML='$'+ intToString( inputPipeline.value, 2);
-        
-        
     }
 }
 
@@ -145,38 +87,19 @@ inputPipeline.onkeyup= function(){
 inputWins.onkeyup= function(){
     
     if( inputWins.value < 1){
-        if(template=="template1"){
-            winsValue1.innerHTML='&#8213';
-            winsValue1.style.color='#a0a2a4';
-            winsBar1.style.opacity='25%';
-            winsBar1.style.width='100%';
-            percentage1.style.opacity='0';
-            percentageImg1.style.opacity='10%';
-        }
-        
-            removeAll();
-            //  winsValue.innerHTML='NO WINS';
-            winsValue.innerHTML='&#8213';
-            winsValue.style.color='#a0a2a4';
-            winContainer.style.textAlign= 'center';
-            winsValue.style.opacity='90%';
-            //winsBar.style.opacity='25%';
-            //winsBar.style.width='100%';
-            percentage.style.opacity='0';
-            percentageImg.style.opacity='25%';
-        
-       
+        removeAll();
+        //  winsValue.innerHTML='NO WINS';
+        winsValue.innerHTML='&#8213';
+        winsValue.style.color='#a0a2a4';
+        winContainer.style.textAlign= 'center';
+        winsValue.style.opacity='90%';
+        //winsBar.style.opacity='25%';
+        //winsBar.style.width='100%';
+        percentage.style.opacity='0';
+        percentageImg.style.opacity='25%';
     }
     else{
-        if(template=="template1"){
-            winsBar1.style.opacity='125%';
-            winsValue1.innerHTML='WINS | $'+ intToString(inputWins.value,2);
-            var percent= ( inputWins.value / inputPipeline.value ) * 100;
-            winsBar1.style.width=percent+'%';
-            winsBar1.style.maxWidth='100%';
-        }
-        
-            removeAll();
+        removeAll();
         //winsBar.style.opacity='125%';
         //winsValue.innerHTML='WINS | $'+ intToString(inputWins.value,2);
         winsValue.innerHTML='$'+ intToString(inputWins.value,2);
@@ -185,8 +108,6 @@ inputWins.onkeyup= function(){
         var percent= ( inputWins.value / inputPipeline.value ) * 100;
         //winsBar.style.width=percent+'%';
         //winsBar.style.maxWidth='100%';
-        
-        
 
         if(inputTarget.value >0){
         percentage.style.opacity='100%';
@@ -204,29 +125,16 @@ inputWins.onkeyup= function(){
 inputLosses.onkeyup= function(){
     
     if( inputLosses.value < 1){
-        if(template=="template1"){
-            lossesValue1.innerHTML='&#8213';
-            lossesValue1.style.color='#a0a2a4';
-            lossesBar1.style.opacity='25%';
-            lossesBar1.style.width='100%';
-        }
         removeAll();
        // lossesValue.innerHTML='NO LOSSES';
        lossesValue.innerHTML='&#8213';
        lossesValue.style.color='#a0a2a4';
        lossesContainer.style.textAlign="center";
         lossesValue.style.opacity='90%';
-        
+        lossesBar.style.opacity='25%';
+        lossesBar.style.width='100%';
     }
     else{
-        if(template=="template1"){
-            lossesBar1.style.opacity='125%';
-            lossesValue1.innerHTML='LOSSES | $'+ intToString(inputLosses.value,2);
-            lossesValue1.style.color='black';
-            var percent= ( inputLosses.value / inputPipeline.value ) * 100;
-            lossesBar1.style.width=percent+'%';
-            lossesBar1.style.maxWidth='100%';
-        }
         removeAll();
         //lossesBar.style.opacity='125%';
         //lossesValue.innerHTML='LOSSES | $'+ intToString(inputLosses.value,2);
@@ -234,7 +142,9 @@ inputLosses.onkeyup= function(){
         lossesValue.style.color='#ff5757';
         lossesContainer.style.textAlign="start";
         
-        
+        var percent= ( inputLosses.value / inputPipeline.value ) * 100;
+        lossesBar.style.width=percent+'%';
+        lossesBar.style.maxWidth='100%';
     }
 }
 
@@ -243,15 +153,6 @@ inputLosses.onkeyup= function(){
 inputTarget.onkeyup=function(){
     
     if(inputTarget.value < 1){
-        if(template=="template1"){
-            targetValue1.style.opacity='0%';
-            targetValue1.innerHTML="&#8213";
-            targetValue1.style.color='#a0a2a4';
-        
-            bullEye1.style.opacity='0%';
-            percentage1.style.opacity='0';
-            percentageImg1.style.opacity='10%';
-        }
         targetValue.style.opacity='0%';
         targetValue.innerHTML="&#8213";
         targetValue.style.color='#a0a2a4';
@@ -261,14 +162,6 @@ inputTarget.onkeyup=function(){
         percentageImg.style.opacity='10%';
     }
     else{
-        if(template=="template1"){
-            targetValue1.style.opacity='100%';
-            targetValue1.style.color='black';
-            bullEye1.style.opacity='100%';
-            targetValue1.innerHTML='$'+ intToString(inputTarget.value,2); 
-
-            percentage1.style.opacity='100%';
-        }
         targetValue.style.opacity='100%';
         targetValue.style.color='black';
         bullEye.style.opacity='100%';
@@ -277,15 +170,11 @@ inputTarget.onkeyup=function(){
         percentage.style.opacity='100%';
         if(inputWins.value >0){
         percentage.innerHTML= ((inputWins.value / inputTarget.value) * 100).toFixed(2)+"%";
-        percentage1.innerHTML= ((inputWins.value / inputTarget.value) * 100).toFixed(2)+"%";
         percentageImg.style.opacity='100%';
-        percentageImg1.style.opacity="100%";
         }
         else{
             percentage.style.opacity='0';
             percentageImg.style.opacity='10%';  
-            percentage1.style.opacity='0';
-            percentageImg1.style.opacity='10%'; 
         }
 
     }
@@ -295,26 +184,19 @@ inputTarget.onkeyup=function(){
 inputTask.onkeyup=function(){
     
     if(inputTask.value < 1){
-        if(template=="template1"){
-            //taskValue1.style.opacity="0%";
-            taskImg1.style.opacity='10%';
-        }
-        //taskValue.style.opacity='0%';
+       // taskValue.style.opacity='0%';
         taskImg.style.opacity='10%';
     }
     else{
-        if(template=="template1"){
-            taskImg1.style.opacity='100%';
-        //taskValue1.innerHTML= intToString(inputTask.value,2); 
-        
-        }
        // taskValue.style.opacity='100%';
         taskImg.style.opacity='100%';
-       // taskValue.innerHTML= intToString(inputTask.value,2); 
-        
+        taskValue.innerHTML= intToString(inputTask.value,2); 
+        if(taskValue.innerHTML <10){
+            taskValue.innerHTML="0"+taskValue.innerHTML;
+        }
     }
 }
-/*
+
 //task
 inputTask.onkeyup=function(){
     
@@ -331,49 +213,41 @@ inputTask.onkeyup=function(){
         }
     }
 }
-*/
+
 //strategy
 strategyYes.onclick=function(){
     
     if(strategyYes.checked == true){
         strategyImg.style.opacity="100%";
-        strategyImg1.style.opacity="100%";
     }
     else{
         strategyImg.style.opacity="10%";
-        strategyImg1.style.opacity="10%";
     }
 }
 strategyNo.onclick=function(){
     
     if(strategyNo.checked == true){
         strategyImg.style.opacity="10%";
-        strategyImg1.style.opacity="10%";
     }
     else{
         strategyImg.style.opacity="100%";
-        strategyImg1.style.opacity="100%";
     }
 }
 
 //competitor
 competitorYes.onclick=function(){
         competitorImg.style.opacity="100%";
-        competitorImg1.style.opacity="100%";
     }
 competitorNo.onclick=function(){
     
      competitorImg.style.opacity="10%";
-     competitorImg1.style.opacity="10%";
     
 }
 
 //status
 status1.onclick=function(){
-    box2.style.border=" 1px solid "+colorStatus;
-    box2.style.background=hexToRGB(colorStatus);
-    box1.style.border=" 1px solid "+colorStatus;
-    box1.style.background=hexToRGB(colorStatus);
+    box.style.border=" 1px solid "+colorStatus;
+    box.style.background=hexToRGB(colorStatus);
     cube.style.display="none";
     circle.style.display="none";
     adjust.style.display="none";
@@ -387,10 +261,8 @@ status1.onclick=function(){
     document.documentElement.style.setProperty("--selection-background", colorStatus);
 }
 status2.onclick=function(){
-    box1.style.border=" 1px solid "+colorStatus;
-    box1.style.background="white";
-    box2.style.border=" 1px solid "+colorStatus;
-    box2.style.background="white";
+    box.style.border=" 1px solid "+colorStatus;
+    box.style.background="white";
     cube.style.display="inline-block";
     cube.style.color=colorStatus;
     circle.style.display="none";
@@ -404,10 +276,8 @@ status2.onclick=function(){
     document.documentElement.style.setProperty("--selection-background", 'transparent');
 }
 status3.onclick=function(){
-    box2.style.border=" 1px dashed "+colorStatus;
-    box2.style.background="white";
-    box1.style.border=" 1px dashed "+colorStatus;
-    box1.style.background="white";
+    box.style.border=" 1px dashed "+colorStatus;
+    box.style.background="white";
     circle.style.display="inline-block";
     circle.style.color=colorStatus;
     cube.style.display="none";
@@ -421,10 +291,8 @@ status3.onclick=function(){
     document.documentElement.style.setProperty("--selection-background", 'transparent');
 }
 status4.onclick=function(){
-    box2.style.border=" 1px solid "+colorStatus;
-    box2.style.background="white";
-    box1.style.border=" 1px solid "+colorStatus;
-    box1.style.background="white";
+    box.style.border=" 1px solid "+colorStatus;
+    box.style.background="white";
     adjust.style.display="inline-block";
     adjust.style.color=colorStatus;
     circle.style.display="none";
@@ -443,11 +311,8 @@ status4.onclick=function(){
 space1.onclick=function(){
     spaceTitle.innerHTML="WHITESPACE";
     spaceTitle.style.color="black";
-    spaceTitle1.innerHTML="WHITESPACE";
-    spaceTitle1.style.color="black";
     spaceTitle.style.background="transparent";
     spaceTitle.style.border="solid 0.5px #000000";
-    spaceTitle1.style.background="transparent";
     
 }
 space2.onclick=function(){
@@ -455,10 +320,6 @@ space2.onclick=function(){
     spaceTitle.style.color="white";
     spaceTitle.style.background="black";
     spaceTitle.style.border="solid 0.5px #000000";
-    spaceTitle1.innerHTML="BLACKSPACE";
-    spaceTitle1.style.color="white";
-    spaceTitle1.style.background="black";
-    
    
 
    
@@ -467,9 +328,6 @@ space3.onclick=function(){
     spaceTitle.style.color="transparent";
     spaceTitle.style.background="transparent";
     spaceTitle.style.border="none";
-    spaceTitle1.style.color="transparent";
-    spaceTitle1.style.background="transparent";
-    spaceTitle1.style.border="none";
     
 }
 
@@ -521,14 +379,9 @@ function hexToRGB(h) {
   function removeAll(){
       if(inputWins.value< 1 && inputLosses.value < 1 && inputPipeline.value < 1){
         allContainer.style.opacity="0";
-        var x=document.getElementById("bowl");
-            x.style.opacity = '0';
-       
       }
       else{
           allContainer.style.opacity="100";
-          var x=document.getElementById("bowl");
-          x.style.opacity = '100';
       }
   }
 
